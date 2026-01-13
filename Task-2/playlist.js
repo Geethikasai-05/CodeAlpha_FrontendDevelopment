d6ab38f9e81fdc64cdd90927a05f00efd7b07e5f
Geethikasai-05 <geethikasai532008@gmail.com>
2026-01-13 13:01:39 +0530

    Create playlist.js

---
+++ Task-2/playlist.js
@@ -0,0 +1,62 @@
+const songs = [
+    {
+      name : "AASA KOODA",
+      artist : "SAI ABHYANKKAR",
+      img : "image-1",
+      audio : "music-1"
+    },
+    {
+      name : "SANCHARI",
+      artist : "SHRUTHI HAASAN, M.M.KEERAVANI",
+      img : "image-2",
+      audio : "music-2"
+    },
+    {
+      name : "GEHRA HUA",
+      artist : "ARJIT SINGH, SHASHWAT SACHDEV",
+      img : "image-3",
+      audio : "music-3"
+    },
+    {
+      name : "CHIKIRI CHIKIRI",
+      artist : "A.R.RAHMAN, MOHIT CHAUHAN",
+      img : "image-4",
+      audio : "music-4"
+    },
+    {
+      name : "MONICA",
+      artist : "ANIRUDH RAVICHANDER",
+      img : "image-5",
+      audio : "music-5"
+    },
+    {
+      name : "CHINNI GUNDELLO",
+      artist : "MERVIN SOLOMON, SATYA YAMINI",
+      img : "image-6",
+      audio : "music-6"
+    },
+    {
+      name : "CHUTAMALLE",
+      artist : "SHILPA RAO",
+      img : "image-7",
+      audio : "music-7"
+    },
+    {
+      name : "VIBE UNDI",
+      artist : "ARMAAN MALIK",
+      img : "image-8",
+      audio : "music-8"
+    },
+    {
+      name : "GOLDEN SPARROW",
+      artist : "MASSTAMILAN",
+      img : "image-9",
+      audio : "music-9"
+    },
+    {
+      name : "BUJJI THALLI",
+      artist : "JAVED ALI - MASS TAMILAN",
+      img : "image-10",
+      audio : "music-10"
+    }
+];
